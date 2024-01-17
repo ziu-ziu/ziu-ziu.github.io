92ebb3e5638b92c471ef4e277346e30e3f98bff7
ziu-ziu <107388202+ziu-ziu@users.noreply.github.com>
2024-01-17 16:37:32 +0900

    add detail.js
    스크롤 이벤트, 닫기 버튼 공통화

---
+++ js/detail.js
@@ -0,0 +1,20 @@
+//스크롤이벤트
+function scrollEvent(elem){
+    window.addEventListener('scroll', function(){
+    const target = document.getElementById(elem);
+    const clientRect = target.getBoundingClientRect();
+    const currentY = window.scrollY;
+    const absouteTop = clientRect.top + currentY;
+    
+    if (absouteTop < currentY){
+        document.getElementById('detail').classList.add("active");
+    }else {
+        document.getElementById('detail').classList.remove("active");
+    }
+})
+}
+
+//닫기
+document.getElementById('close').addEventListener('click', (e) => {
+    window.history.back();
+})
